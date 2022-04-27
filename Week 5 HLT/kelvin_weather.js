cffe623691b25724f19839680ebdb97bf22d6454
Ad-Mason <103945797+Ad-Mason@users.noreply.github.com>
2022-04-27 11:00:35 +0100

    Update kelvin_weather.js

--- Week 5 HLT/kelvin_weather.js
+++ Week 5 HLT/kelvin_weather.js
@@ -1,7 +1,23 @@
 //creating var to to show todays temp in Kelvin
 const kelvin = 293;
-//creating new var to show todays temp in Celsius
+
+
+//creating new var to show todays temp in Celsius by using value of Kelvin
 let celsius = kelvin - 273;
-console.log(celsius);
+
+
+//creating new var to show todays temp in Fahrenheit calculated from value of celcius
+let fahrenheit = Celsius*(9/5)+32;
+
+
+//.floor() method applied to fahrenheit to round down fahrenheit to integer 
+fahrenheit.floor() = fahrenheit
+
+
+console.log('The temperature is ${fahrenheit} degrees Fahrenheit.')
+
+
+
+
 
 
