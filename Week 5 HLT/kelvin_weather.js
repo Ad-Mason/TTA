375306569a1e0bd69362093bd7ed4af4d0ba80d7
Ad-Mason <103945797+Ad-Mason@users.noreply.github.com>
2022-04-27 10:52:11 +0100

    Update kelvin_weather.js

--- Week 5 HLT/kelvin_weather.js
+++ Week 5 HLT/kelvin_weather.js
@@ -1,5 +1,6 @@
-//creating var to have value of todays temp
+//creating var to to show todays temp in Kelvin
 const kelvin = 293;
+//creating new var to show todays temp in Celsius
 let celsius = kelvin - 273;
 console.log(celsius);
 
