b07209714938b2aa8b609b98720bb739c06508bb
Ad-Mason <103945797+Ad-Mason@users.noreply.github.com>
2022-04-28 14:08:24 +0100

    Update Magic_Eight_Ball.js

--- Week 5 HLT/Magic_Eight_Ball.js
+++ Week 5 HLT/Magic_Eight_Ball.js
@@ -6,10 +6,10 @@ if (userName.length > 0) {
     console.log("Hello!");
 }
 
-let userQuestion ="Who's going to win the World Cup"
+let userQuestion ="Will England win the World Cup"
 
 if (userQuestion.length > 0 && userName.length > 0) {
-    console.log(`${userName} asks, ${userQuestion}?`);
+    console.log(`${userName} asks: ${userQuestion}?`);
 } else if (userQuestion.length > 0 && userName.length < 1) {
     console.log(`${userQuestion}?`);
 } else if (userQuestion.length > 1 && userName.length > 0) {
@@ -17,4 +17,43 @@ if (userQuestion.length > 0 && userName.length > 0) {
 } else {
        console.log("Please ask a question");
    } 
+
+let randomNumber = Math.floor(Math.random() * 7);
+
+console.log(randomNumber)
+
+let eightBall = ""
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+   eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+}
+
+
+
+console.log(`The Magic 8 Ball answered: ${eightBall}`);
+
+
 
