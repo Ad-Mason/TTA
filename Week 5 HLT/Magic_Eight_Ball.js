b45a8bd6139233e4e3d1e8941c9fe3d1a033c126
Ad-Mason <103945797+Ad-Mason@users.noreply.github.com>
2022-04-28 10:03:28 +0100

    Update

---
+++ Week 5 HLT/Magic_Eight_Ball.js
@@ -0,0 +1,9 @@
+let userName =""
+
+if (userName.length > 0) {
+    console.log(`Hello, ${userName}!`);
+} else {
+    console.log("Hello!");
+}
+
+let userQuestion
--- Week 5 HLT/kelvin_weather.js
+++ Week 5 HLT/kelvin_weather.js
@@ -1,10 +1,9 @@
 //creating var to to show todays temp in Kelvin
-const kelvin = 293;
+const kelvin = 0;
 
 
 //creating new var to show todays temp in Celsius by using value of Kelvin
 let celsius = kelvin - 273;
-console.log(celsius)
 
 //creating new var to show todays temp in Fahrenheit calculated from value of celcius
 let fahrenheit = celsius*(9/5)+32;
@@ -13,9 +12,8 @@ let fahrenheit = celsius*(9/5)+32;
 //.floor() method applied to fahrenheit to round down fahrenheit to integer 
 fahrenheit = Math.floor (fahrenheit);
 
-
 //logging the temp in Fahrenheit using string interpolation and the fahrenheit var
-console.log('The temperature is ${fahrenheit} degrees Fahrenheit');
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
 
 
 
