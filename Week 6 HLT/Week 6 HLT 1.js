ed2ea021745580797ed56121ee89fa975cb14097
A-Rival <100780770+A-Rival@users.noreply.github.com>
2022-05-06 12:53:28 +0100

    Week 6

---
+++ Week 6 HLT/Week 6 HLT 1.js
@@ -0,0 +1,113 @@
+
+var temperature
+var condition
+var goToWork
+
+// temperature = prompt("what is the temperature?")
+
+// condition = prompt("what is the weather condition?")
+
+temperature = 9
+
+condition = "sunny"
+
+ //when temperature is over 10 the following condition will set the boolean value of goToWork
+
+ if(temperature > 10)  {
+
+  switch (condition) {
+     case "sunny" :
+    goToWork = true
+   break;
+     case "cloudy":
+    goToWork = true
+   break;
+     case "raining" :
+    goToWork = false
+   break;
+     case "snowing" :
+    goToWork = false
+   break;
+     case "thunder" :
+    goToWork = false
+   break;   }
+
+  } 
+
+  //when temperature is over 20 the following condition will set the boolean value of goToWork 
+
+    else if (temperature > 20){
+
+    switch (condition) {
+      case "sunny" :
+     goToWork = true
+    break;
+      case "cloudy":
+     goToWork = true
+    break;
+      case "raining" :
+     goToWork = true
+    break;
+      case "snowing" :
+     goToWork = false
+    break;
+      case "thunder" :
+     goToWork = false
+    break;   }
+
+  }     
+
+    //when temperature is over 30 the following condition will set the boolean value of goToWork 
+  
+    else if (temperature > 30){
+      
+    switch (condition) {
+      case "sunny" :
+     goToWork = false
+    break;
+      case "cloudy":
+     goToWork = false
+    break;
+      case "raining" :
+     goToWork = false
+    break;
+      case "snowing" :
+     goToWork = false
+    break;
+      case "thunder" :
+     goToWork = false
+    break;   }
+
+  }
+
+    //when temperature is under 10 the following condition will set the boolean value of goToWork 
+
+    else if (temperature < 10){
+      
+    switch (condition) {
+      case "sunny" :
+     goToWork = false
+    break;
+      case "cloudy":
+     goToWork = false
+    break;
+      case "raining" :
+     goToWork = false
+    break;
+      case "snowing" :
+     goToWork = false
+    break;
+      case "thunder" :
+     goToWork = false
+    break;   }
+
+    }
+
+
+    //Use goToWork value to display statement whether Kelvin should or shouldnn't go work
+
+  if (goToWork) {
+  console.log("Get to work Kelvin, you crazy bastard!")
+  } else {
+   console.log("Don't go to work Kelvin, it's not safe.")
+  } 
