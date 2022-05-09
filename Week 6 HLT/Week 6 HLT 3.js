26ec43a07d775ed81340e5edce6544240470cb5e
Ad-Mason <103945797+Ad-Mason@users.noreply.github.com>
2022-05-09 13:52:23 +0100

    Update Week 6 HLT 3.js

--- Week 6 HLT/Week 6 HLT 3.js
+++ Week 6 HLT/Week 6 HLT 3.js
@@ -1,10 +1,10 @@
-// let firstValue = prompt("Enter a number")
-// let secondValue = prompt("Enter a second number")
-// let operation = prompt("Enter operator to perform the calculation ( either +, -, * or / ): ");
+let firstValue = prompt("Enter a number")
+let secondValue = prompt("Enter a second number")
+let operation = prompt("Enter operator to perform the calculation ( either +, -, * or / ): ");
 
-let firstValue = 1;
-let secondValue = 2;
-let operation = "^";
+// let firstValue = 1;
+// let secondValue = 2;
+// let operation = "^";
 
 function multiplication(firstValue, secondValue) { 
     return firstValue * secondValue;
