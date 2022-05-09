46fd2a7c246424d63dc7fc569a4b7341f9c2fe57
A-Rival <100780770+A-Rival@users.noreply.github.com>
2022-05-09 17:50:20 +0100

    Updates

--- Week 6 HLT/Week 6 HLT 1.js
+++ Week 6 HLT/Week 6 HLT 1.js
@@ -7,13 +7,60 @@ var goToWork
 
 // condition = prompt("what is the weather condition? ( either sunny, clowdy, raining, snowing or thunder")
 
-temperature = 21
+temperature = 25
 
-condition = "Sunny"
+condition = "snowing"
 
- //when temperature is over 10 the following condition will set the boolean value of goToWork
+    //when temperature is over 30 and under 30 the following condition will set the boolean value of goToWork 
+  
+  if (temperature > 30){
+      
+      switch (condition.toLowerCase()) {
+        case "sunny" :
+       goToWork = false
+      break;
+        case "cloudy":
+       goToWork = false
+      break;
+        case "raining" :
+       goToWork = false
+      break;
+        case "snowing" :
+       goToWork = false
+      break;
+        case "thunder" :
+       goToWork = false
+      break;   }
+  
+    }
 
- if(temperature > 10)  {
+    //when temperature is over 20 the following condition will set the boolean value of goToWork 
+
+    else if (temperature > 20){
+
+      switch (condition.toLowerCase()) {
+        case "sunny" :
+       goToWork = true
+      break;
+        case "cloudy":
+       goToWork = true
+      break;
+        case "raining" :
+       goToWork = true
+      break;
+        case "snowing" :
+       goToWork = false
+      break;
+        case "thunder" :
+       goToWork = false
+      break;   }
+  
+    }     
+  
+
+ //when temperature is 10 or above the following condition will set the boolean value of goToWork
+
+  else if(temperature >= 10)  {
 
   switch (condition.toLowerCase()) {
      case "sunny" :
@@ -34,51 +81,6 @@ condition = "Sunny"
 
   } 
 
-  //when temperature is over 20 the following condition will set the boolean value of goToWork 
-
-    else if (temperature > 20){
-
-    switch (condition.toLowerCase()) {
-      case "sunny" :
-     goToWork = true
-    break;
-      case "cloudy":
-     goToWork = true
-    break;
-      case "raining" :
-     goToWork = true
-    break;
-      case "snowing" :
-     goToWork = false
-    break;
-      case "thunder" :
-     goToWork = false
-    break;   }
-
-  }     
-
-    //when temperature is over 30 the following condition will set the boolean value of goToWork 
-  
-    else if (temperature > 30){
-      
-    switch (condition.toLowerCase()) {
-      case "sunny" :
-     goToWork = false
-    break;
-      case "cloudy":
-     goToWork = false
-    break;
-      case "raining" :
-     goToWork = false
-    break;
-      case "snowing" :
-     goToWork = false
-    break;
-      case "thunder" :
-     goToWork = false
-    break;   }
-
-  }
 
     //when temperature is under 10 the following condition will set the boolean value of goToWork 
 
@@ -106,8 +108,22 @@ condition = "Sunny"
 
     //Use goToWork value to display statement whether Kelvin should or shouldnn't go work
 
-  if (goToWork) {
-  console.log("Get to work Kelvin, you crazy bastard.")
-  } else {
-   console.log("Don't go to work Kelvin, it's not safe.")
+  if (goToWork && condition == "raining" && temperature > 20) {
+      console.log("Being warm and wet isn't the worst, Kelvin")
+  }
+    else if (goToWork) {
+      console.log("It's safe to continue working on your new lab today, Kelvin")
+
+  } else if (temperature < 10){
+      console.log("It's too cold to work effectively today, Kelvin")
+
+  } else if (condition = "snowing" && temperature > 20) {
+      console.log("The weather is too wierd today, Kelvin")
+
+  } else if (temperature > 30){
+      console.log("It's too hot to work effectively today, Kelvin")
+
+  } else if (condition = "thunder" && temperature > 20) {
+      console.log("It's not safe to work today, Kelvin")
+
   } 
--- Week 6 HLT/Week 6 HLT 2.js
+++ Week 6 HLT/Week 6 HLT 2.js
@@ -5,17 +5,18 @@ var fruits = ["Apple", "Orange", "Banana", "Pear", "Peach", "Strawberry", "Cherr
 
 
 for (let fruit in fruits)  {
-    //console.log(fruit)
+
+
     var vowels = 0
     var consonants = 0
-    //console.log(fruit.length)
+    
     for (var i = 0; i < fruits[fruit].length; i++){
        if (fruits[fruit][i].toLowerCase() == "a" || fruits[fruit][i].toLowerCase() == "e" || fruits[fruit][i].toLowerCase() == "i" || fruits[fruit][i].toLowerCase() == "o" || fruits[fruit][i].toLowerCase() == "u") {
            vowels ++
        } else {
            consonants ++
        }
-       //console.log(fruits[fruit][i])
+
     }
     console.log(`${fruits[fruit]} has ${vowels} vowels and ${consonants} consonants`)
 } 
