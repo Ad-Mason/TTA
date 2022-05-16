00cd5d7af9642dd1ca83d63607a6e8d9c2e979f3
A-Rival <100780770+A-Rival@users.noreply.github.com>
2022-05-16 22:41:54 +0100

    update

--- Week 7 HLT/A-Rival Website/SRC/JavaScript/index.js
+++ Week 7 HLT/A-Rival Website/SRC/JavaScript/index.js
@@ -1,8 +1,37 @@
 
 function submitBlog(){
-    
+
+  //check title is entered or alert
+
+  var searchtitle = document.getElementById("Title").value;
+  if(searchtitle=='')
+  {
+      alert('Enter the title');
+      return false;
+  }
+
+  //check author is entered or alert
+
+  var searchauthor = document.getElementById("Author").value;
+  if(searchauthor=='')
+  {
+      alert('Enter the author');
+      return false;
+  }
+
+  //check message is entered or alert
+
+  var searchpost = document.getElementById("Post").value;
+  if(searchpost=='')
+  {
+      alert('Enter message');
+      return false;
+  }
+  
+  //create elements to uploat to blog 
+
   var title = document.createElement("h2");
-  var author = document.createElement("h2");
+  var author = document.createElement("h4");
   var post = document.createElement("p");
   title.innerText = document.getElementById("Title").value;
   author.innerText = document.getElementById("Author").value;
@@ -10,8 +39,10 @@ function submitBlog(){
   document.getElementById("myBlog").appendChild(title);
   document.getElementById("myBlog").appendChild(author);
   document.getElementById("myBlog").appendChild(post);
-}
+  }
 
+
+//toggle diplay 
 
 function toggle() {
     var elementB = document.body;
