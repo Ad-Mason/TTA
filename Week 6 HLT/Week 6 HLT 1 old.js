1421d1c21ec43b4b04e7b73371bea309dc11a826
A-Rival <100780770+A-Rival@users.noreply.github.com>
2022-05-16 07:37:34 +0100

    update

---
+++ Week 6 HLT/Week 6 HLT 1 old.js
@@ -0,0 +1,117 @@
+
+//create variables
+
+var temperature
+var condition
+var goToWork
+var continuousAnalysis = "true"
+
+do {   (function()  {
+
+//ask user to input tmeperature and weather condition
+
+temperature = prompt("What is the current temperature in degrees celsius?");
+
+condition = prompt("What is the current weather condition? Please enter either sunny, cloudy, raining, snowing or thunder");
+
+
+  //when temperature is over 30 the following condition will set the boolean value of goToWork 
+  
+if (temperature > 30){
+
+       goToWork = false
+  
+    }
+
+  //when temperature is over 20 the following condition will set the boolean value of goToWork 
+
+  else if (temperature > 20){
+
+      switch (condition.toLowerCase()) {
+        case "sunny" :
+       goToWork = true
+      break;
+        case "cloudy":
+       goToWork = true
+      break;
+        case "raining" :
+       goToWork = true
+      break;
+        case "snowing" :
+       goToWork = false
+      break;
+        case "thunder" :
+       goToWork = false
+      break;   }
+  
+    }     
+  
+
+  //when temperature is 10 or above the following condition will set the boolean value of goToWork
+
+  else if(temperature >= 10)  {
+
+     switch (condition.toLowerCase()) {
+       case "sunny" :
+     goToWork = true
+     break;
+       case "cloudy":
+     goToWork = true
+     break;
+       case "raining" :
+     goToWork = false
+     break;
+       case "snowing" :
+     goToWork = false
+     break;
+       case "thunder" :
+      goToWork = false
+     break;   }
+
+    } 
+
+
+    //when temperature is under 10 the following condition will set the boolean value of goToWork 
+
+    else if (temperature < 10){
+
+     goToWork = false;
+
+     }
+
+  
+
+
+    //Use goToWork value to display statement whether Kelvin should or shouldnn't go work
+
+  if (goToWork && condition == "raining" && temperature > 20) {
+      console.log("Being warm and wet isn't the worst, you can continue working on your new lab today, Kelvin");
+  }
+    else if (goToWork) {
+      console.log("You can continue working on your new lab today, Kelvin");
+
+  } else if (temperature < 10){
+      console.log("It's too cold to work effectively today, Kelvin");
+
+  } else if (condition == "snowing" && temperature > 20) {
+      console.log("The weather is too wierd today, Kelvin");
+
+  } else if (temperature > 30){
+      console.log("It's too hot to work effectively today, Kelvin");
+
+  } else if (condition == "thunder" && temperature >= 10) {
+      console.log("It's not safe to work today, Kelvin");
+
+  } else {
+    console.log("Sorry that weather condition is not recognised");
+  }
+  
+
+
+
+}())  
+}
+
+
+while(continuousAnalysis);
+
--- Week 6 HLT/Week 6 HLT 1.js
+++ Week 6 HLT/Week 6 HLT 1.js
@@ -5,111 +5,125 @@ var temperature
 var condition
 var goToWork
 var continuousAnalysis = "true"
+var message
 
 do {   (function()  {
 
-//ask user to input tmeperature and weather condition
+//request user inputs
 
 temperature = prompt("What is the current temperature in degrees celsius?");
 
 condition = prompt("What is the current weather condition? Please enter either sunny, cloudy, raining, snowing or thunder");
 
+//evaluate weather condition + temperature to set gotowork and message values
 
-  //when temperature is over 30 the following condition will set the boolean value of goToWork 
-  
-if (temperature > 30){
-
-       goToWork = false
-  
+switch(condition.toLowerCase()){
+  case "sunny" :
+    if (temperature >30) {
+      goToWork = false;
+      message = 5;
+    } else if (temperature >20) {
+      goToWork = true;
+      message = 1;
+    } else if (temperature <10) {
+      goToWork = false;
+      message = 6;
+    } else if (temperature >=10) {
+      goToWork = true;
+      message = 1;
     }
-
-  //when temperature is over 20 the following condition will set the boolean value of goToWork 
-
-  else if (temperature > 20){
-
-      switch (condition.toLowerCase()) {
-        case "sunny" :
-       goToWork = true
-      break;
-        case "cloudy":
-       goToWork = true
-      break;
-        case "raining" :
-       goToWork = true
-      break;
-        case "snowing" :
-       goToWork = false
-      break;
-        case "thunder" :
-       goToWork = false
-      break;   }
-  
-    }     
-  
-
-  //when temperature is 10 or above the following condition will set the boolean value of goToWork
-
-  else if(temperature >= 10)  {
-
-     switch (condition.toLowerCase()) {
-       case "sunny" :
-     goToWork = true
-     break;
-       case "cloudy":
-     goToWork = true
-     break;
-       case "raining" :
-     goToWork = false
-     break;
-       case "snowing" :
-     goToWork = false
-     break;
-       case "thunder" :
+    break;
+  case "cloudy" :
+    if (temperature >30) {
+      goToWork = false;
+      message = 5;
+    } else if (temperature >20) {
+      goToWork = true;
+      message = 1;
+    } else if (temperature <10) {
+      goToWork = false;
+      message = 6;
+    } else if (temperature >=10) {
+      goToWork = true;
+      message = 1;
+    }
+    break;
+  case "raining" :
+    if (temperature >30) {
+      goToWork = false;
+      message = 5;
+    } else if (temperature >20) {
+      goToWork = true;
+      message = 3;
+    } else if (temperature <10) {
+      goToWork = false;
+      message = 6;
+    } else if (temperature >=10) {
+      goToWork = false;
+      message = 7;
+    }
+    break;
+  case "snowing" :
+    if (temperature >30) {
+      goToWork = false;
+      message = 4;
+    } else if (temperature >20) {
       goToWork = false
-     break;   }
-
-    } 
-
-
-    //when temperature is under 10 the following condition will set the boolean value of goToWork 
-
-    else if (temperature < 10){
-
-     goToWork = false;
-
-     }
-
-  
-
-
-    //Use goToWork value to display statement whether Kelvin should or shouldnn't go work
-
-  if (goToWork && condition == "raining" && temperature > 20) {
-      console.log("Being warm and wet isn't the worst, you can continue working on your new lab today, Kelvin");
-  }
-    else if (goToWork) {
-      console.log("You can continue working on your new lab today, Kelvin");
-
-  } else if (temperature < 10){
-      console.log("It's too cold to work effectively today, Kelvin");
-
-  } else if (condition == "snowing" && temperature > 20) {
-      console.log("The weather is too wierd today, Kelvin");
-
-  } else if (temperature > 30){
-      console.log("It's too hot to work effectively today, Kelvin");
-
-  } else if (condition == "thunder" && temperature >= 10) {
-      console.log("It's not safe to work today, Kelvin");
-
-  } else {
-    console.log("Sorry that weather condition is not recognised");
-  }
-  
+      message = 4
+    } else if (temperature <10) {
+      goToWork = false;
+      message = 6;
+    } else if (temperature >=10) {
+      goToWork = false
+      message = 2
+    }
+    break;
+  case "thunder" :
+    if (temperature >30) {
+      goToWork = false;
+      message = 5;
+    } else if (temperature >20) {
+      goToWork = false;
+      message = 2;
+    } else if (temperature <10) {
+      goToWork = false;
+      message = 6;
+    } else if (temperature >=10) {
+      goToWork = false;
+      message = 2;
+    }
+    break;
+}
 
 
+//output message 
 
-}())  
+switch(message){
+  case 1:
+    console.log("You can continue working on your new lab today, Kelvin");
+  break;
+  case 2:
+    console.log ("It's not safe to work today, Kelvin");
+  break;
+  case 3:
+    console.log("You can continue working on your new lab today, Kelvin, as being warm and wet isn't the worst");
+  break;
+  case 4:
+    console.log("The weather is too weird today, Kelvin");
+  break;
+  case 5:
+    console.log("It's too hot to work effectively today, Kelvin");
+  break;
+  case 6:
+    console.log("It's too cold to work effectively today, Kelvin");
+  break;
+  case 7:
+    console.log("No one likes being wet and cold, Kelvin");
+  default:
+    return "That weather condition is not recognised";
+}
+
+}())
 }
 
 
